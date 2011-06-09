589b7e506441a4e4fc982fd8547d875d5e98981a
Henrik Hodne <dvyjones@dvyjones.com>
2011-06-09 00:10:26 -0700

    Vanilla Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CluePics::Application.config.secret_token = '5d94cffa929b30b1738b1ee9c94c28fb3dc6d0ab419db97c89d922e2530bd9f6a79a572aa32d6d95cc923adccc8959a1a66e39ebbee02f75188ed3c70591a23e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CluePics::Application.config.session_store :cookie_store, key: '_CluePics_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CluePics::Application.config.session_store :active_record_store
